844687acf254417c8f7e34b0be8e03083284de76
Crystal Maldonado <howdycrystalm@gmail.com>
2020-01-05 20:31:09 -0800

    Added div to wrap post info

--- wordpress/wp-content/themes/wordpress-theme-starter-master/loop.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/loop.php
@@ -1,39 +1,42 @@
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
+<!-- Dynamically adding alt text to variable -->
+		<?php $thumbnail_id  = get_post_thumbnail_id( $post->ID ); ?>
+		<?php $thumbnail_alt = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true );?>
+
 	<!-- article -->
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 		<!-- post thumbnail -->
 		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-				<!-- <?php the_post_thumbnail('feat-img'); // Declare pixel size you need inside the array ?> -->
 
-				<!-- <? $thumbnail_id = get_post_thumbnail_id( $post->ID );
-						$alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);   
-						the_post_thumbnail( 'custom-size', array( 'alt' => $alt ) ); ?> -->
-
-				<div class="placeholder" style="background-image: url(<?php echo the_post_thumbnail_url();?>);" >
-          <img src="<?php echo get_template_directory_uri();?>/img/clear-placeholders/small.png">  
+				<div class="placeholder bottom-line" style="background-image: url(<?php echo the_post_thumbnail_url();?>);" >
+          <img src="<?php echo get_template_directory_uri();?>/img/clear-placeholders/small.png" alt="<?php echo $thumbnail_alt ?>">  
         </div>
 			</a>
 		<?php endif; ?>
 		<!-- /post thumbnail -->
 
-		<!-- post title -->
-		<h2>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-		</h2>
-		<!-- /post title -->
+		<!-- info box -->
+		<div class="info">
+			<!-- post title -->
+			<h2 class="title">
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+			</h2>
+			<!-- /post title -->
 
-		<!-- post details -->
-		<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-		<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-		<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-		<!-- /post details -->
+			<!-- post details -->
+			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
+			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
+			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
+			<!-- /post details -->
 
-		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+			<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
 
-		<?php edit_post_link(); ?>
+			<?php edit_post_link(); ?>
+		</div>
+		<!-- info box -->
 
 	</article>
 	<!-- /article -->
